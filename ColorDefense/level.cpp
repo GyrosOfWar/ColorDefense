f5510afb689137b2114354780bcaeceb57a02934
Martin Tomasi <martin.tomasi@gmail.com>
2013-07-11 13:21:18 +0200

    added loadFromFile to level

--- ColorDefense/level.cpp
+++ ColorDefense/level.cpp
@@ -30,3 +30,26 @@ int level::getLvlno(void) {
 bool level::isFinished(void) {
 	return finished;
 }
+
+bool level::loadFromFile(const std::string& path) {
+	sf::Image levelImg;
+	if(!levelImg.loadFromFile(path))
+		return false;
+	
+	unsigned int N = levelImg.getSize().x;
+	unsigned int M = levelImg.getSize().y;
+	for(unsigned int i = 0; i < N; i++) {
+		for(unsigned int j = 0; j < M; j++) {\
+			sf::Color c = levelImg.getPixel(i, j);
+			if(c == sf::Color::Black)
+				logger->info("Buildable tile");
+			if(c == sf::Color::White)
+				logger->info("Walkable tile");\
+			if(c == sf::Color::Green)
+				logger->info("Start tile");
+			if(c == sf::Color::Red)
+				logger->info("End tile");
+		}
+	}
+	return true;
+}
