ff1e1beccf04f62b24f5f59a290c8dff666fa78b
Martin Tomasi <martin.tomasi@gmail.com>
2013-07-19 14:24:13 +0200

    Increased enemy move speed by 2x Cleaned up some project files

--- ColorDefense/constants.hpp
+++ ColorDefense/constants.hpp
@@ -1,21 +1,21 @@
 #pragma once
 #include <string>
-using namespace std;
-namespace game {
-	
-const int SCREENHEIGHT = 600;
-const int SCREENWIDTH = 800;
-const float ASPECT_RATIO = static_cast<float>(SCREENWIDTH) / static_cast<float>(SCREENHEIGHT);
-const int CELLX = 16;
-const int CELLY = 12;
-const int TILEHEIGHT = SCREENHEIGHT/CELLY;
-const int TILEWIDTH = SCREENWIDTH/CELLX;
-const int FPS = 20;
-const string BASE_PATH = "res/levels/level";
-const string TILES_SUFFIX = "tiles.png";
-const string LEVELFILE_SUFFIX = "lvl.png";
-// This doesn't seem right
-const float MOVE_DISTANCE = static_cast<float>(TILEHEIGHT) / static_cast<float>(FPS);
 
+using namespace std;
+
+namespace game {
+
+	const int SCREENHEIGHT = 600;
+	const int SCREENWIDTH = 800;
+	const float ASPECT_RATIO = static_cast<float>(SCREENWIDTH) / static_cast<float>(SCREENHEIGHT);
+	const int CELLX = 16;
+	const int CELLY = 12;
+	const int TILEHEIGHT = SCREENHEIGHT/CELLY;
+	const int TILEWIDTH = SCREENWIDTH/CELLX;
+	const int FPS = 30;
+	const string BASE_PATH = "res/levels/level";
+	const string TILES_SUFFIX = "tiles.png";
+	const string LEVELFILE_SUFFIX = "lvl.png";
+	const float MOVE_DISTANCE = static_cast<float>(TILEHEIGHT*2) / static_cast<float>(FPS);
 
 }
